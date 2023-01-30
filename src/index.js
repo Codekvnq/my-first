be41edf70b74eda44e8735900668198939d98f88
codekvnq <108369423+Codekvnq@users.noreply.github.com>
2023-01-29 21:30:48 -0700

    my-first react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const createdElement = (
+//   <h1>
+//     <span>Hi</span>
+//     <br></br>
+//     <span>Hello</span>
+//   </h1>
+// );
+function Header() {
+  return <header>
+    <span>Hello</span>
+    <ul>
+      <li>Home</li>
+    </ul>
+  </header> 
+}
+function Footer() {
+  
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header/>);
